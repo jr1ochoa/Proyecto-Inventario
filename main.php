27d419383cf0e8e6671a923421064ce998979cef
Jairojramos <30820953+Jairojramos@users.noreply.github.com>
2023-12-06 09:11:40 -0600

    Add files via upload

---
+++ main.php
@@ -0,0 +1,14 @@
+<!-- Banner -->
+<div id="banner">
+    <article data-position="bottom right">
+        <div class="inner">
+            <img src="images/banner01.jpg" alt="">
+            <div class="features">
+                <a href="?view=login" class="accent1 alt">
+                    <h2>Sistema de <br/>Información Institucional</h2>
+                    <p>Accede al sistema con tu usuario y contraseña</p>
+                </a>
+            </div>
+        </div>
+    </article>
+</div>
