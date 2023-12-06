5805a0ad2eb5126a8e8a0cea93d3635e0dfa1ac1
Jairojramos <30820953+Jairojramos@users.noreply.github.com>
2023-12-06 09:08:51 -0600

    Add files via upload

---
+++ template.php
@@ -0,0 +1,68 @@
+<?php
+
+    $query = ""
+
+?>
+
+<!-- Main -->
+<div id="main">
+    <div class="inner">
+
+    <!-- Content -->
+        <div id="content">
+
+            <!-- Posts -->
+                <section>
+                    <header>
+                        <h1>Bienvenido </h1>
+                        <p>Dolor sit amet adipiscing consequat veroeros</p>
+                    </header>
+                    <p>Phasellus dapibus convallis scelerisque. Donec tempus augue id tortor ultricies eget sed pellentesque turpis ultrices. Vestibulum ut nulla sem, vel iaculis arcu. Nulla vel lorem nisl, quis ultricies tellus. Nunc imperdiet elit sed dolore lacus venenatis urna dolor est sagittis laoreet. Phasellus dapibus convallis scelerisque. Donec tempus augue id tortor ultricies eget pellentesque turpis ultrices. Vestibulum ut nulla sem.</p>
+                    <section class="posts">
+                        <article>
+                            <div class="image fit"><a href="#"><img src="images/pic01.jpg" alt="" /></a></div>
+                            <h2>Magna lorem feugiat</h2>
+                            <p>Nulla sem, vel iaculis arcu. Nulla vel lorem nisl quis sed ultricies amet tellus magna feugiat.</p>
+                            <ul class="actions fixed">
+                                <li><a href="#" class="button">Learn More</a></li>
+                            </ul>
+                        </article>
+                    </section>
+                </section>
+
+        </div>
+
+    <!-- Sidebar -->
+        <div id="sidebar">
+
+        <!-- Features -->
+            <section class="features">
+                <a href="#" class="accent1">
+                    <h2>Amet est phasellus</h2>
+                    <p>Phasellus amet lorem et magna donec nullam augue tortor.</p>
+                </a>
+            </section>
+
+        <!-- Mini Posts (small) -->
+            <section>
+                <header>
+                    <h2>Ipsum sed feugiat</h2>
+                </header>
+                <section class="mini-posts small">
+                    <article>
+                        <div class="image"><a href="#"><img src="images/pic08.jpg" alt="" /></a></div>
+                        <div class="content">
+                            <a href="#">Nulla sem, vel iaculis arcu magna et tempus consequat.</a>
+                            <div class="timestamp">8 hours ago</div>
+                        </div>
+                    </article>
+                        
+                    <ul class="actions fixed">
+                        <li><a href="#" class="button">More Posts</a></li>
+                    </ul>
+                </section>
+            </section>
+
+        </div>
+    </div>
+</div>
